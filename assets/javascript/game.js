0b4f72b22b70ab48b5ac2864af430e3ce79b73a3
Rafael Galvez <rfgalvez@msn.com>
2019-07-18 22:57:06 -0400

    added global vars

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+//global variable
+var targetNumber= 100
+var counter=0;
+var randomNumber=Math.floor(Math.random()*100+30)
+$(document).ready(function(){
+    $("#midoriya").on("click", function() {
+
+        counter += 40;
+        console.log("midoriya")
+    })})
