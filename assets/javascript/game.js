46e53cfbf435cf4fb471512911aabd7363be7090
Rafael Galvez <rfgalvez@msn.com>
2019-07-21 19:32:38 -0400

    java

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,43 @@
 //global variable
+$(document).ready(function(){
 var targetNumber= 100
 var counter=0;
 var randomNumber=Math.floor(Math.random()*100+30)
-$(document).ready(function(){
-    $("#midoriya").on("click", function() {
+var num1=Math.floor(Math.random()*11+1)
+var num2=Math.floor(Math.random()*11+1)
+var num3=Math.floor(Math.random()*11+1)
+var num4=Math.floor(Math.random()*11+1)
+var wins=0
+var loses=0
+var playerTotal=0
+$("#totalScore").text(playerTotal)
+$("#wins").text(wins)
+$("#losses").text(loses)
 
-        counter += 40;
-        console.log("midoriya")
-    })})
+function great(){
+    win++
+    $("#wins").text(wins)
+}
+
+function boo() {
+    loses++
+    $("#losses").text(loses);
+}
+
+$("#one").on('click', function() {
+    playerTotal= playerTotal +num1;
+    console.log("New userTotal= " + playerTotal)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+)})
