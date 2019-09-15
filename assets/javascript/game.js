4f5300a986a7340d81c9f6238b1923f7a1b14382
Rafael Galvez <rfgalvez@msn.com>
2019-09-15 18:49:24 -0400

    added on click function. made a reset function. got it to work. lack of css though

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,110 @@
 //global variable
 $(document).ready(function(){
-var targetNumber= 100
 var counter=0;
-var randomNumber=Math.floor(Math.random()*100+30)
-var num1=Math.floor(Math.random()*11+1)
-var num2=Math.floor(Math.random()*11+1)
-var num3=Math.floor(Math.random()*11+1)
-var num4=Math.floor(Math.random()*11+1)
-var wins=0
-var loses=0
-var playerTotal=0
-$("#totalScore").text(playerTotal)
-$("#wins").text(wins)
-$("#losses").text(loses)
+//creating random number to be guessed
+var randomNumber=Math.floor(Math.random()*100+30);
+//creating random numbers
+var num1=Math.floor(Math.random()*11+1);
+var num2=Math.floor(Math.random()*11+1);
+var num3=Math.floor(Math.random()*11+1);
+var num4=Math.floor(Math.random()*11+1);
 
-function great(){
-    win++
-    $("#wins").text(wins)
-}
+var wins=0;
+var loses=0;
 
-function boo() {
-    loses++
-    $("#losses").text(loses);
+//player total set to 0
+var playerTotal=0;
+
+$("#totalScore").text(playerTotal);
+$("#wins").text("Wins: " + wins);
+$("#losses").text("Losses: " + loses);
+$("#score").text("Goal: " + randomNumber);
+
+function resetGame() {
+    playerTotal= 0;
+    randomNumber=Math.floor(Math.random()*100+30);
+    num1=Math.floor(Math.random()*11+1);
+    num2=Math.floor(Math.random()*11+1);
+    num3=Math.floor(Math.random()*11+1);
+    num4=Math.floor(Math.random()*11+1);
+    $("#score").text("Goal: " + randomNumber);
+    $("#totalScore").text(playerTotal);
+    
 }
 
 $("#one").on('click', function() {
     playerTotal= playerTotal +num1;
-    console.log("New userTotal= " + playerTotal)
+    $("#totalScore").text(playerTotal);
+    console.log("New userTotal= " + playerTotal);
+    
+    if(playerTotal > randomNumber){
+        loses++;
+        console.log("inside if - losses: " + loses);
+        $("#losses").text("Losses: " + loses);
+        resetGame()
+    } else if(playerTotal ===randomNumber) {
+        wins ++;
+        $("#wins").text("Wins: " + wins);
+        resetGame();
+    }
+})
+
+
+
+
+$("#two").on('click', function() {
+    playerTotal= playerTotal +num2;
+    $("#totalScore").text(playerTotal);
+    console.log("New userTotal= " + playerTotal);
+
+    if(playerTotal > randomNumber){
+        loses++;
+        console.log("inside if - losses: " + loses);
+        $("#losses").text("Losses: " + loses);
+        resetGame();
+    } else if(playerTotal ===randomNumber) {
+        wins ++;
+        $("#wins").text("Wins: " + wins);
+        resetGame();
+    }
+    
+})
+
+
+$("#three").on('click', function() {
+    playerTotal= playerTotal +num3;
+    $("#totalScore").text(playerTotal);
+    console.log("New userTotal= " + playerTotal);
+
+    if(playerTotal > randomNumber){
+        loses++;
+        console.log("inside if - losses: " + loses);
+        $("#losses").text("Losses: " + loses);
+        resetGame();
+    } else if(playerTotal ===randomNumber) {
+        wins ++;
+        $("#wins").text("Wins: " + wins);
+        resetGame();
+    }
+    
+})
+
+
+
+$("#four").on('click', function() {
+    playerTotal= playerTotal +num4;
+    $("#totalScore").text(playerTotal)
+    console.log("New userTotal= " + playerTotal);
+
+    if(playerTotal > randomNumber){
+        loses++;
+        console.log("inside if - losses: " + loses);
+        $("#losses").text("Losses: " + loses);
+        resetGame();
+    } else if(playerTotal ===randomNumber) {
+        wins ++;
+        $("#wins").text("Wins: " + wins);
+        resetGame();
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
 )})
